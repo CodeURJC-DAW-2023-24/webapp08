94c77d9e4ad46198900f8d5dea2bc2fe78bfc5ac
v-orzaez <victoriaorzaezdelacruz@hotmail.com>
2024-03-28 11:46:34 +0100

    Open API in RestLogin

--- backend/src/main/java/com/example/backend/controller/RESTLoginController.java
+++ backend/src/main/java/com/example/backend/controller/RESTLoginController.java
@@ -8,10 +8,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.backend.model.Exercise;
 import com.example.backend.security.jwt.AuthResponse;
 import com.example.backend.security.jwt.AuthResponse.Status;
 import com.example.backend.security.jwt.LoginRequest;
 import com.example.backend.security.jwt.UserLoginService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -22,6 +29,13 @@ public class RESTLoginController {
 	@Autowired
 	private UserLoginService userService;
 
+	@Operation(summary = "Login ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Correct login", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))
+            }),
+            @ApiResponse(responseCode = "500", description = "Error", content = @Content)
+    })
 	@PostMapping("/login")
 	public ResponseEntity<AuthResponse> login(
 			@CookieValue(name = "accessToken", required = false) String accessToken,
@@ -31,6 +45,14 @@ public class RESTLoginController {
 		return userService.login(loginRequest, accessToken, refreshToken);
 	}
 
+
+	@Operation(summary = "Refresh login ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Correct refresh", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))
+            }),
+            @ApiResponse(responseCode = "401", description = "You are not logged", content = @Content)
+    })
 	@PostMapping("/refresh")
 	public ResponseEntity<AuthResponse> refreshToken(
 			@CookieValue(name = "refreshToken", required = false) String refreshToken) {
@@ -38,6 +60,13 @@ public class RESTLoginController {
 		return userService.refresh(refreshToken);
 	}
 
+	@Operation(summary = "Logout ")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Correct logout", content = {
+                    @Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponse.class))
+            }),
+            @ApiResponse(responseCode = "401", description = "You are not logged", content = @Content)
+    })
 	@PostMapping("/logout")
 	public ResponseEntity<AuthResponse> logOut(HttpServletRequest request, HttpServletResponse response) {
 
