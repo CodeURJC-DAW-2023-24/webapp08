464603179b73b5842349ed741b392f05d6a745dd
Ruben-Rodriguez-Redondo <r.rodriguezr.2020@alumnos.urjc.es>
2024-03-30 18:11:12 +0100

    Fixed some errors in the pom and dockerfile

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker login
-docker build -t rubenrr/webapp08:1.0 -f docker/Dockerfile .
-docker push rubenrr/webapp08:1.0
+docker build -t rubenrr/webapp08 -f docker/Dockerfile .
+docker push rubenrr/webapp08
