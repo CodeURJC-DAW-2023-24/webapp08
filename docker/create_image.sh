4a89bd22ab9d9524fc943eb25676c092f7b21bf1
Ruben-Rodriguez-Redondo <r.rodriguezr.2020@alumnos.urjc.es>
2024-03-31 14:36:12 +0200

    Modified create_image.sh so that you must add a version as an argument

--- docker/create_image.sh
+++ docker/create_image.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Por favor, proporciona un nombre de versión como argumento."
+    exit 1
+else
+    VERSION=$1
+fi
 docker login
-docker build -t rubenrr/webapp08 -f docker/Dockerfile .
-docker push rubenrr/webapp08
+docker build -t rubenrr/webapp08:$VERSION -f docker/Dockerfile .
+docker push rubenrr/webapp08:$VERSION
