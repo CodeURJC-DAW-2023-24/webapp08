ec26b44e783896c4a34f2dfe6fc86434927de6b1
Ruben-Rodriguez-Redondo <r.rodriguezr.2020@alumnos.urjc.es>
2024-04-15 12:41:30 +0200

    Fixed z-index in loginimage

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login.component';
+import { HeaderComponent } from './header.component';
 
 const routes: Routes = [
-  {}
+
+  {path:'/mainPage', component: HeaderComponent},
+  {path: '/login', component: LoginComponent }
+
 ];
 
 @NgModule({
