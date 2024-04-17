85b7635ffbce89feacda521d8cb54774f7d2ccd3
Ruben-Rodriguez-Redondo <r.rodriguezr.2020@alumnos.urjc.es>
2024-04-17 18:38:57 +0200

    Added bar chart (with fixed data)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { NgApexchartsModule } from "ng-apexcharts";
 
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './header.component';
@@ -24,7 +25,8 @@ import { StatisticsComponent } from './statistics.component';
     AppRoutingModule,
     NgbModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgApexchartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
